5792dcac9e6f192c2fc8f3b6d66cf700a43422c4
Nuria <nuria@codegram.com>
2019-12-12 17:41:05 +0100

    footer links

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,19 +1,41 @@
 <template>
   <footer class="footer container">
-    <a href="https://twitter.com/pincfloit">Twitter</a>
-    ·
-    <a href="http://github.com/nuria-fl/">Github</a>
-    ·
-    <a href="https://nuria.itch.io/">Itch.io</a>
-    ·
-    <a href="https://medium.com/@pincfloit">Medium</a>
-    ·
-    <a href="https://dev.to/pincfloit">Dev.to</a>
+    <template v-for="link, i in links">
+      <a :href="link.url" rel="noopener noreferrer nofollow" :key="link.name">{{ link.name }}</a>
+      <template v-if="i < links.length - 1">&nbsp; · &nbsp;</template>
+    </template>
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: "Twitter",
+          url: "https://twitter.com/pincfloit"
+        },
+        {
+          name: "GitHub",
+          url: "http://github.com/nuria-fl/"
+        },
+        {
+          name: "Itch.io",
+          url: "https://nuria.itch.io/"
+        },
+        {
+          name: "Medium",
+          url: "https://medium.com/@pincfloit"
+        },
+        {
+          name: "Dev.to",
+          url: "https://dev.to/pincfloit"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
