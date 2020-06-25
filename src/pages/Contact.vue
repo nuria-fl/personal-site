1787abd737ec7b1126a0b966425bbd6d7abbcf79
Nuria <nuria@codegram.com>
2020-06-25 17:59:53 +0200

    add contact form

---
+++ src/pages/Contact.vue
@@ -0,0 +1,70 @@
+<template>
+  <section class="container container--sm">
+    <h1>Contact</h1>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label
+          >Name: <input class="input" type="text" name="name" required
+        /></label>
+      </p>
+      <p>
+        <label
+          >Email: <input class="input" type="email" name="email" required
+        /></label>
+      </p>
+      <p>
+        <label
+          >Message:
+          <textarea class="input" name="message" required></textarea>
+        </label>
+      </p>
+      <p class="checkbox">
+        <label>
+          <input type="checkbox" required />
+          I have read and accept the
+          <g-link to="/privacy">privacy policy</g-link>.
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: var(--title-size);
+  margin-bottom: 2rem;
+}
+.input {
+  display: block;
+  width: 100%;
+  margin: 0.5rem 0;
+  border: 3px solid $secondary-color;
+  padding: 0.5rem;
+  color: #555;
+  font-size: 1.2rem;
+  font-family: "PT Serif", serif;
+}
+.checkbox {
+  font-size: 1rem;
+}
+button {
+  min-width: 10rem;
+  padding: 0.5rem;
+  border: none;
+  font-family: "PT Serif", serif;
+  background: $primary-color;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.3s;
+  &:hover {
+    background: $primary-text-color;
+  }
+}
+</style>
---
+++ src/pages/Privacy.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="container">
+    <h1>
+      Privacy Policy
+    </h1>
+    <p>
+      I have no interest in using any information other than to get back to you
+      if you reach out via the <g-link to="/contact">contact form</g-link>.
+    </p>
+
+    <h2>Collection of your personal information</h2>
+    <p>
+      This site only asks for information when you submit the form. The form
+      submission is handled by
+      <a href="https://www.netlify.com/" rel="noopener noreferrer">Netlify</a>.
+      You can read their
+      <a href="https://www.netlify.com/privacy/" rel="noopener noreferrer"
+        >privacy policy</a
+      >
+      and
+      <a
+        href="https://docs.netlify.com/forms/setup/#html-forms"
+        rel="noopener noreferrer"
+        >form documentation</a
+      >
+      for more information about how they handle data.
+    </p>
+    <p>
+      There are no analytics or any other form of tracking, that's why you don't
+      see any cookie notice either.
+    </p>
+
+    <h2>Sharing of your personal information</h2>
+
+    <p>
+      I will not share any information you submit via the form. For more
+      information on how Netlify handles your data, visit their
+      <a href="https://www.netlify.com/privacy/" rel="noopener noreferrer"
+        >privacy policy</a
+      >.
+    </p>
+
+    <h2>Use of your personal information</h2>
+    <p>
+      If your reach out to me via <g-link to="/contact">the form</g-link> to get
+      in contact with me, will only use your email to contact you.
+    </p>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: var(--title-size);
+  margin-bottom: 2rem;
+}
+</style>
