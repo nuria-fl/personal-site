69076c8639c21dae5ad81e36491db4ffba95592d
Núria <nuria.soriano@voicemod.net>
2025-03-23 15:51:00 +0100

    add projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,5 +13,15 @@ export const collections = {
       image: z.string(),
       featured: z.boolean().optional().default(false)
     })
+  }),
+  projects: defineCollection({
+    type: 'content', // markdown files
+    schema: z.object({
+      title: z.string(),
+      image: z.string(),
+      category: z.string(),
+      link: z.string().url(),
+      date: z.coerce.date()
+    })
   })
 }; 
